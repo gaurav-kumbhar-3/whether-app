b10349ded9517e077bbd2b9af1e159e03dc87ff1
gaurav-kumbhar-3 <kumbhar.3008@gmail.com>
2026-06-22 17:27:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const apiKey = "f302afc05d06388b860d5c7863af5804";
+
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", () => {
+
+  const city = document.getElementById("cityInput").value;
+
+  getWeather(city);
+
+});
+
+async function getWeather(city){
+
+  const url =
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  try{
+
+    const response = await fetch(url);
+
+    if(!response.ok){
+      throw new Error("City not found");
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+    
+
+    document.getElementById("cityName").innerText =
+      data.name;
+
+    document.getElementById("temperature").innerText =
+      `Temperature: ${data.main.temp}°C`;
+
+    document.getElementById("description").innerText =
+      `Condition: ${data.weather[0].description}`;
+
+    document.getElementById("humidity").innerText =
+      `Humidity: ${data.main.humidity}%`;
+
+    document.getElementById("wind").innerText =
+      `Wind Speed: ${data.wind.speed} km/h`;
+
+  }
+  catch(error){
+
+    alert(error.message);
+
+  }
+
+}
+
+
+
+
+
+
+
